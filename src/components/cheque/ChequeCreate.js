d73e9146a85279a5d33d0f4fc9d101b226e2a80c
Youssef IHRAI <y.ihrai@atner.net>
2023-04-13 13:20:59 +0000

    correction eureur

--- src/components/cheque/ChequeCreate.js
+++ src/components/cheque/ChequeCreate.js
@@ -1,29 +1,33 @@
 import { useEffect, useState } from "react";
 import {
   AutocompleteArrayInput,
-  AutocompleteInput,
-  Create,
-  DateInput,
-  regex,
-  required,
-  SelectInput,
-  SimpleForm,
-  TextInput,
+    AutocompleteInput,
+    Create,
+
+    DateInput,
+
+    required,
+
+    SelectInput,
+
+    SimpleForm,
+
+    TextInput,
+
+    useDataProvider,
 } from "react-admin";
-
 import { makeStyles } from "@material-ui/styles";
-import { Chip } from "@material-ui/core";
 
+import { Chip } from "@material-ui/core";
 const useStyles = makeStyles(() => ({
-  autocomplete: {
-    width: "650px",
-  },
-  chip: {
-    fontWeight: "bold",
-  },
+    autocomplete: {
+        width: "650px",
+    },
+    chip: {
+        fontWeight: "bold",
+    },
 }));
-export const ChequeCreate = () => {
- 
+export const ChequeCreate = (props) => {
   const [orderVirement, setOrderVirement] = useState([
     {
       id: "null",
@@ -32,165 +36,83 @@ export const ChequeCreate = () => {
       etat: "",
     },
   ]);
-  const [fournisseur, setFournisseur] = useState([
-    {
-      CodeFournisseur: "",
-      nom: "",
-      id: 0,
-      FournisseurId: 0,
-      rib: "",
-      validation: "",
-    },
-  ]);
-  const [ribFournisseur, setRibFournisseur] = useState([
-    {
-      CodeFournisseur: "",
-      nom: "",
-      id: 0,
-      FournisseurId: 0,
-      rib: "",
-      validation: "",
-    },
-  ]);
-  const [facture, setFacture] = useState([
-    {
-      id: "",
-      DATEDOC: "1960-01-01T00:00:00.000Z",
-      nom: "",
-      NETAPAYER: 0.0,
-      MontantFacture: 0.0,
-    },
-  ]);
+    const [fournisseur, setFournisseur] = useState([]);
+    const [facture, setFacture] = useState([{ id: "", BonCommande: "" }]);
+    const dataProvider = useDataProvider();
 
+    const [fournisseurIdField, setFournisseurIdField] = useState(true);
 
+    function formatDate(string) {
+        var options = { year: "numeric", month: "long", day: "numeric" };
+        return new Date(string).toLocaleDateString([], options);
+    }
+   
   const [sum, setSum] = useState("0.000");
-
-  const [orderVirementField, setOrderVirementField] = useState(true);
-  const [fournisseurIdField, setFournisseurIdField] = useState(true);
-
-
-  const [onchangefournisseur, setOnchangefournisseur] = useState([]);
-
-  const validatecheque = regex(
-    /^[a-zA-Z]+ [0-9]{8}$/,
-    "ce  numero de cheque est invalid "
-    );
+   
   useEffect(() => {
-    fetch("http://10.111.1.95:8080/ribatner")
+    fetch("http://localhost:8080/ribatner")
       .then((response) => response.json())
       .then((json) => setOrderVirement(json));
   }, []);
 
-  // const ribf = useRef([0]);
 
-  useEffect(() => {
-    // console.log("after use effect", fournisseur);
-    if (onchangefournisseur > 0) {
-      setRibFournisseur(
-        fournisseur.filter((fournisseur) => {
-          return fournisseur.FournisseurId === onchangefournisseur;
-        })
-      );
-      let filterRib = fournisseur.filter((fournisseur) => {
-        return fournisseur.FournisseurId === onchangefournisseur;
-      });
-      setRibFournisseur(filterRib);
-
-      // let filterFactureByFournisseur = facture.filter((facture) => {
-      //   return facture.nom === ribFournisseur[0].nom;
-      // });
-      // setFactureFilter(filterFactureByFournisseur);
-    }
-  }, [onchangefournisseur, fournisseur]);
-
-  const getFactureByFourniseurId = (id) => {
-    let url = "http://10.111.1.95:8080/getfacturebyfournisseurid/" + id;
-    // console.log(url);
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        // console.log(json);
-        setFacture(json);
-      });
-    // console.log(facture);
-  };
-
-  const getFournisseurFilteredByOv = () => {
-    fetch(
-      `http://10.111.1.95:8080/fournisseurs/`
-    )
-      .then((response) => response.json())
-      .then((json) => setFournisseur(json));
-  };
-  let orderVirement_choices = orderVirement.map(({ id,nom }) => ({
-    id: id,
-    name: nom,
-  }));
-  let fournisseurs_choices = fournisseur.map(
-    ({ FournisseurId,nom,CodeFournisseur}) => ({
-      id: FournisseurId,
-      name: `${CodeFournisseur}     ${nom} `,
-    })
-  );
-
-
-
-  let facture_choices = facture.map(
-    ({
-      id,
-      chantier,
-      nom,
-      ficheNavette,
-      DateFacture,
-      CODEDOCUTIL,
-      TTC,
-      MontantFacture,
-    }) => ({
+    useEffect(() => {
+        dataProvider
+            .getList("fournisseurs", {
+                pagination: { page: 1, perPage: 3000 },
+                sort: { field: "nom", order: "ASC" },
+            })
+            .then(({ data }) => {
+                setFournisseur(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [dataProvider]);
+    const getFactureByFourniseur = (id) => {
+        let url = "http://10.111.1.95:8080/getfacturebyfournisseurid/" + id;
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => setFacture(json));
+    };
+    let facture_choices = { id: "", BonCommande: "" };
+    let fournisseurs_choices = fournisseur.map(({ id, nom, CodeFournisseur }) => ({
+        id: id,
+        name: `${nom} | ${CodeFournisseur} `,
+    }));
+    facture_choices = facture.map(({  id,
+        chantier,
+        nom,
+        ficheNavette,
+        DateFacture,
+        CODEDOCUTIL,
+        TTC,
+        MontantFacture, }) => ({
+        id: id,
+        name: `${CODEDOCUTIL} | ${chantier} | FN ${ficheNavette} | ${
+            DateFacture?.split("T")[0]
+          } | ${nom} |${MontantFacture != null ? MontantFacture : TTC}`,
+    }));
+    
+    let orderVirement_choices = orderVirement.map(({ id,nom }) => ({
       id: id,
-      name: `${CODEDOCUTIL} | ${chantier} | FN ${ficheNavette} | ${
-        DateFacture?.split("T")[0]
-      } | ${nom} |${MontantFacture != null ? MontantFacture : TTC}`,
-    })
-  );
-
-  const classes = useStyles();
-  return (
-    <Create>
-      <SimpleForm>
-        <SelectInput
+      name: nom,
+    }));
+    
+    const classes = useStyles();
+    return (
+        <Create>
+            <SimpleForm>
+            <SelectInput
           validate={required("Ce champ est obligatoire")}
           className={classes.autocomplete}
           source="orderVirementId"
           label ="banque"
-          onChange={(e) => {
-            // console.log(e.target.value);
-            if (e.target.value === "") {
-              setOrderVirementField(true);
-            } else {
-              setOrderVirementField(false);
-              getFournisseurFilteredByOv(e.target.value);
-            }
-          }}
+         
           choices={orderVirement_choices}
         />
-        <AutocompleteInput
-          validate={required("Ce champ est obligatoire")}
-          disabled={orderVirementField}
-          className={classes.autocomplete}
-          source="fournisseurId"
-          choices={fournisseurs_choices}
-          onChange={(e) => {
-            setOnchangefournisseur(e);
-            getFactureByFourniseurId(e);
-            // console.log(e);
-            if (!e) {
-              setFournisseurIdField(true);
-            } else {
-              setFournisseurIdField(false);
-            }
-          }}
-        />
-     <DateInput source="datecheque" label="datecheque"  className={classes.autocomplete}></DateInput> 
+              
+    <DateInput source="datecheque" label="datecheque"  className={classes.autocomplete}></DateInput> 
 
      <DateInput source="dateecheance" label="dateecheance"  className={classes.autocomplete}></DateInput> 
      
@@ -201,7 +123,25 @@ export const ChequeCreate = () => {
      className={classes.autocomplete}>
       
       </TextInput> 
-        <AutocompleteArrayInput
+              
+                <AutocompleteInput label="Fournisseur"
+                    validate={required("Le fournisseur est obligatoire")}
+                    className={classes.autocomplete}
+                    source="fournisseurId"
+                    choices={fournisseurs_choices}
+                    onChange={
+                        (e) => {
+                            // setOnchangefournisseur(e);
+                            if (!e) {
+                                setFournisseurIdField(true);
+                            } else {
+                                setFournisseurIdField(false);
+                                getFactureByFourniseur(e);
+                            }
+                        }
+                    }
+                />
+                <AutocompleteArrayInput
           validate={required("Ce champ est obligatoire")}
           disabled={fournisseurIdField}
           className={classes.autocomplete}
@@ -221,7 +161,9 @@ export const ChequeCreate = () => {
           }}
         />
         <Chip className={classes.chip} label={`Total : ${sum}`} />
-      </SimpleForm>
-    </Create>
-  );
+
+                
+            </SimpleForm>
+        </Create>
+    );
 };
