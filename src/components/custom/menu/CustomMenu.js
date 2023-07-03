f100cddc39ce9004040a723578103fe21fa09dbd
youssefihrai <49070572+youssefihrai@users.noreply.github.com>
2023-07-03 15:30:18 +0100

    traitement des avances

--- src/components/custom/menu/CustomMenu.js
+++ src/components/custom/menu/CustomMenu.js
@@ -113,9 +113,6 @@ export const CustomMenu = (props) => {
         />
       )}
 
-
-
-
  {(permissions === "admin" ||
         permissions === "normal user" ||
         permissions === "superviseur comptabilite" ||
