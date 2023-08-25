a106727a5694e3979879d4045f23756c9fe09030
youssefihrai <49070572+youssefihrai@users.noreply.github.com>
2023-08-25 10:46:31 +0100

    corriger print

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = "http://10.111.1.9:8080"; 
+const apiUrl = "http://10.111.1.95:8080"; 
 //const apiServer = "http://10.111.1.95:8080"
 //const apiUrllocal = "http://localhost:8080"
 export default apiUrl;
