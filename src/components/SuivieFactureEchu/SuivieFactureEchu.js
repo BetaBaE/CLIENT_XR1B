a457598751c58d15f5c976a551e46c640c5199cc
youssefihrai <49070572+youssefihrai@users.noreply.github.com>
2023-10-18 12:19:37 +0100

    Echeance facture

--- src/components/SuivieFactureEchu/SuivieFactureEchu.js
+++ src/components/SuivieFactureEchu/SuivieFactureEchu.js
@@ -4,28 +4,29 @@ import AllechuFilter from "./SuivieFactureFilter";
 
 export const SuivieFactureEchu = (props) => {
   
-  const CustomRowStyle = (record) => {
-    let today = new Date();
+  // const CustomRowStyle = (record) => {
+  //   let today = new Date();
     
-    const futureDate = new Date(record.DateFacture);
-    futureDate.setDate(futureDate.getDate() + 45); 
+  //   const futureDate = new Date(record.DateFacture);
+  //   futureDate.setDate(futureDate.getDate() + 45); 
   
-    let nbDay = (futureDate - today) / (1000 * 24 * 60 * 60);
+  //   let nbDay = (futureDate - today) / (1000 * 24 * 60 * 60);
   
-    console.log(nbDay);
+  //   console.log(nbDay);
   
-    if (nbDay < 0) {
-      return { backgroundColor: "#FFBB33" }; 
-    } else {
-      return { backgroundColor: "#B7BAAF" };
-    }
-  };
+  //   if (nbDay < 0) {
+  //     return { backgroundColor: "#FFBB33" }; 
+  //   } else {
+  //     return { backgroundColor: "#B7BAAF" };
+  //   }
+  // };
   
   return (
 
 
       <List filters={<AllechuFilter />} title="Log Facture Saisie">
-        <Datagrid bulkActionButtons={false} rowStyle={CustomRowStyle} {...props}>
+        {/* <Datagrid bulkActionButtons={false} rowStyle={CustomRowStyle} {...props}> */}
+            <Datagrid bulkActionButtons={false}  {...props}>
           <TextField source="chantier" label="chantier" />
           <TextField source="ficheNavette" label="ficheNavette" />
           <TextField source="designation" label="designation" />
@@ -38,17 +39,12 @@ export const SuivieFactureEchu = (props) => {
           <TextField source="MontantTVA" label="récupération tva" />
           <TextField source="TTC" label="TTC" />
           <TextField source="montantAvance" label="montantAvance" />
-          <DateField showTime source="dateOperation" options={{ timeZone: "UTC" }} label="confirmation relevé"/>
-          <TextField source="modepaiement" label="modepaiement" />
-          <TextField source="banque" label="banque" />
-          <TextField source="numerocheque" label="numerocheque" />
-          <DateField showTime source="datecheque" options={{ timeZone: "UTC" }} />
-          <DateField showTime source="dateecheance" options={{ timeZone: "UTC" }} />
-          <TextField source="etat" label="etat" />
-          {/* <DateField
-          source="dateechu"
-           label="dateechu"
-                              /> */}
+          <TextField source="modalitepaiement" label="modalitepaiement" />
+        
+          <DateField
+          source="DateEcheancePaiement"
+           label="DateEcheance"
+                              /> 
         </Datagrid>
       </List>
      
